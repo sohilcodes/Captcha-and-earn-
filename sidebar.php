3ff77db2959ab669c7ed424adb902f58418e5598
Sohil Khan <96345871+sohilcodes@users.noreply.github.com>
2025-12-11 11:25:14 +0530

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,12 +1,15 @@
 <div id="sidebar" class="sidebar">
     <a href="javascript:void(0)" class="closebtn" onclick="closeMenu()">&times;</a>
 
-    <a href="index.php">Home</a>
-    <a href="earn.php">Earn Captcha</a>
-    <a href="withdraw.php">Withdraw</a>
-    <a href="history.php">History</a>
-    <a href="support.php">Support</a>
-
-    <hr>
-    <a href="logout.php" style="color:red;">Logout</a>
+    <?php if(isset($_SESSION["user"])): ?>
+        <a href="dashboard.php">Dashboard</a>
+        <a href="earn.php">Earn Captcha</a>
+        <a href="withdraw.php">Withdraw</a>
+        <a href="history.php">History</a>
+        <hr>
+        <a href="logout.php" style="color:red;">Logout</a>
+    <?php else: ?>
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+    <?php endif; ?>
 </div>
