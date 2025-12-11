b43901a23d4fc6aee4d55c4a835f948775bb8e35
Sohil Khan <96345871+sohilcodes@users.noreply.github.com>
2025-12-11 11:20:06 +0530

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,12 @@
+<div id="sidebar" class="sidebar">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeMenu()">&times;</a>
+
+    <a href="index.php">Home</a>
+    <a href="earn.php">Earn Captcha</a>
+    <a href="withdraw.php">Withdraw</a>
+    <a href="history.php">History</a>
+    <a href="support.php">Support</a>
+
+    <hr>
+    <a href="logout.php" style="color:red;">Logout</a>
+</div>
